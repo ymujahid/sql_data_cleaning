66a3d40d75e1be35c43ce860753fc08460ef6688
ymujahid <yunusmujahid5@gmail.com>
2025-07-28 10:10:32 +0100

    standardize data

--- layoff.sql
+++ layoff.sql
@@ -65,3 +65,54 @@ DELETE FROM layoffs_staging2 WHERE row_num > 1;
 
 SELECT * FROM layoffs_staging2;
 
+-- Standardizing Data
+
+SELECT company, TRIM(company)
+FROM layoffs_staging2;
+
+update layoffs_staging2
+SET company  = trim(company);
+
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY 1; 
+
+SELECT * 
+FROM layoffs_staging2
+WHERE industry LIKE 'Crypto%';
+
+-- some of the crypto industries were written as Crypto Currency
+-- we need to standardize the crypto industry name
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+SELECT DISTINCT location
+FROM layoffs_staging2
+ORDER BY 1;
+
+SELECT DISTINCT country
+FROM layoffs_staging2
+WHERE country LIKE 'United States%';
+
+SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
+FROM layoffs_staging2
+ORDER by 1;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
+
+SELECT `date`,
+STR_TO_DATE(`date`, '%m/%d/%Y')
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+SELECT *
+FROM layoffs_staging2;
