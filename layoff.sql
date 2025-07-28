8b6d71667c4d218c1a1b78081f654a81c6d6253b
ymujahid <yunusmujahid5@gmail.com>
2025-07-28 10:39:49 +0100

    Remove null values and irrelevant columns

--- layoff.sql
+++ layoff.sql
@@ -116,3 +116,61 @@ MODIFY COLUMN `date` DATE;
 
 SELECT *
 FROM layoffs_staging2;
+
+-- Removing Null and Blank Values
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE company = 'Airbnb';
+
+SELECT *
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+    AND t1.location = t2.location
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+    AND t1.location = t2.location
+SET t1.industry = t2.industry
+WHERE (t1.industry IS NULL)
+AND t2.industry IS NOT NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+-- we cant trust these rows of data for further analysis, we are therefore dropping them
+
+DELETE
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2;
+
+-- delete any columnsin
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+
